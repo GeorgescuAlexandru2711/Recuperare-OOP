136a4da26efb09910a09a14b6a129c381fb7ad2d
GeorgescuAlexandru2711 <georgescu.alexandru.c4v@student.ucv.ro>
2025-06-30 08:17:41 +0300

    Add files via upload

---
+++ OOP2.cpp
@@ -0,0 +1,149 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+#include <string>
+using namespace std;
+
+// Clasa abstractă Produs cu metoda clone inclusă
+class Produs {
+public:
+    virtual double getPret() const = 0;
+    virtual void descriere() const = 0;
+    virtual shared_ptr<Produs> clone() const = 0;
+    virtual ~Produs() {}
+};
+
+// Clasa derivată Mancare
+class Mancare : public Produs {
+private:
+    string denumire;
+    double pret;
+    bool vegetarian;
+public:
+    Mancare(string denumire_, double pret_, bool vegetarian_)
+        : denumire(denumire_), pret(pret_), vegetarian(vegetarian_) {
+    }
+
+    double getPret() const override {
+        return pret;
+    }
+
+    void descriere() const override {
+        cout << "Mancare: " << denumire << ", Pret: " << pret
+            << ", Vegetarian: " << (vegetarian ? "Da" : "Nu") << "\n";
+    }
+
+    shared_ptr<Produs> clone() const override {
+        return make_shared<Mancare>(*this);
+    }
+};
+
+// Clasa derivată Bautura
+class Bautura : public Produs {
+private:
+    string denumire;
+    double pret;
+    bool alcoolica;
+public:
+    Bautura(string denumire_, double pret_, bool alcoolica_)
+        : denumire(denumire_), pret(pret_), alcoolica(alcoolica_) {
+    }
+
+    double getPret() const override {
+        return pret;
+    }
+
+    void descriere() const override {
+        cout << "Bautura: " << denumire << ", Pret: " << pret
+            << ", Alcoolica: " << (alcoolica ? "Da" : "Nu") << "\n";
+    }
+
+    shared_ptr<Produs> clone() const override {
+        return make_shared<Bautura>(*this);
+    }
+};
+
+// Clasa Comanda
+class Comanda {
+private:
+    vector<shared_ptr<Produs>> produse;
+public:
+    void adaugaProdus(shared_ptr<Produs> p) {
+        produse.push_back(p);
+    }
+
+    void adaugaProdus(const Produs& p) {
+        produse.push_back(p.clone());
+    }
+
+    const vector<shared_ptr<Produs>>& getProduse() const {
+        return produse;
+    }
+
+    void afiseazaComanda() const {
+        cout << "Detalii comanda:\n";
+        for (const auto& p : produse) {
+            p->descriere();
+        }
+    }
+
+    // Supraincarcare operator +
+    Comanda operator+(const Comanda& alta) const {
+        Comanda rezultat = *this;
+        for (const auto& p : alta.produse) {
+            rezultat.adaugaProdus(*p);
+        }
+        return rezultat;
+    }
+};
+
+// Template pentru calculul prețului total
+template <typename T>
+class CalculatorPret {
+private:
+    const T& produse;
+public:
+    CalculatorPret(const T& produse_) : produse(produse_) {}
+
+    double calculeazaTotal() const {
+        double total = 0;
+        for (const auto& p : produse) {
+            total += p->getPret();
+        }
+        return total;
+    }
+};
+
+// Funcție principală de test
+int main() {
+    // Creăm produse
+    shared_ptr<Produs> pizza = make_shared<Mancare>("Pizza Margherita", 35.5, true);
+    shared_ptr<Produs> burger = make_shared<Mancare>("Burger Vita", 45.0, false);
+    shared_ptr<Produs> cola = make_shared<Bautura>("Coca-Cola", 8.0, false);
+    shared_ptr<Produs> bere = make_shared<Bautura>("Bere", 12.5, true);
+
+    // Creăm comenzi
+    Comanda comanda1;
+    comanda1.adaugaProdus(pizza);
+    comanda1.adaugaProdus(cola);
+
+    Comanda comanda2;
+    comanda2.adaugaProdus(burger);
+    comanda2.adaugaProdus(bere);
+
+    cout << "Comanda 1:\n";
+    comanda1.afiseazaComanda();
+    cout << "\nComanda 2:\n";
+    comanda2.afiseazaComanda();
+
+    // Combinăm comenzile
+    Comanda comandaFinala = comanda1 + comanda2;
+    cout << "\nComanda Finala (combinata):\n";
+    comandaFinala.afiseazaComanda();
+
+    // Calculăm prețul total
+    CalculatorPret<vector<shared_ptr<Produs>>> calculator(comandaFinala.getProduse());
+    cout << "\nTotal de plata: " << calculator.calculeazaTotal() << " RON\n";
+
+    return 0;
+}
---
+++ OOP3.cpp
@@ -0,0 +1,161 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+#include <string>
+using namespace std;
+
+// Clasa de bază abstractă Document
+class Document {
+public:
+    virtual void citeste() = 0;
+    virtual void afiseaza() const = 0;
+    virtual ~Document() {}
+};
+
+// Clasa Carte derivată din Document
+class Carte : public Document {
+private:
+    string titlu;
+    string autor;
+    int nrPagini;
+public:
+    Carte(const string& titlu_ = "", const string& autor_ = "", int nrPagini_ = 0)
+        : titlu(titlu_), autor(autor_), nrPagini(nrPagini_) {
+    }
+
+    void citeste() override {
+        cout << "Introdu titlul cartii: ";
+        getline(cin, titlu);
+        cout << "Introdu autorul: ";
+        getline(cin, autor);
+        cout << "Introdu numarul de pagini: ";
+        cin >> nrPagini;
+        cin.ignore();
+    }
+
+    void afiseaza() const override {
+        cout << "Carte - Titlu: " << titlu
+            << ", Autor: " << autor
+            << ", Numar pagini: " << nrPagini << "\n";
+    }
+};
+
+// Clasa Revista derivată din Document
+class Revista : public Document {
+private:
+    string titlu;
+    int nrEditie;
+public:
+    Revista(const string& titlu_ = "", int nrEditie_ = 0)
+        : titlu(titlu_), nrEditie(nrEditie_) {
+    }
+
+    void citeste() override {
+        cout << "Introdu titlul revistei: ";
+        getline(cin, titlu);
+        cout << "Introdu numarul editiei: ";
+        cin >> nrEditie;
+        cin.ignore();
+    }
+
+    void afiseaza() const override {
+        cout << "Revista - Titlu: " << titlu
+            << ", Editia nr.: " << nrEditie << "\n";
+    }
+};
+
+// Clasa Ziar derivată din Document
+class Ziar : public Document {
+private:
+    string titlu;
+    string dataAparitie;
+public:
+    Ziar(const string& titlu_ = "", const string& dataAparitie_ = "")
+        : titlu(titlu_), dataAparitie(dataAparitie_) {
+    }
+
+    void citeste() override {
+        cout << "Introdu titlul ziarului: ";
+        getline(cin, titlu);
+        cout << "Introdu data aparitiei: ";
+        getline(cin, dataAparitie);
+    }
+
+    void afiseaza() const override {
+        cout << "Ziar - Titlu: " << titlu
+            << ", Data aparitiei: " << dataAparitie << "\n";
+    }
+};
+
+// Clasa Bibliotecar care gestionează lista de documente
+class Bibliotecar {
+private:
+    vector<shared_ptr<Document>> listaDocumente;
+public:
+    void adaugaDocument(shared_ptr<Document> doc) {
+        listaDocumente.push_back(doc);
+    }
+
+    void afiseazaDocumente() const {
+        cout << "Lista documente:\n";
+        for (const auto& doc : listaDocumente) {
+            doc->afiseaza();
+        }
+    }
+
+    shared_ptr<Document> operator[](size_t index) const {
+        if (index >= listaDocumente.size()) {
+            throw out_of_range("Index invalid!");
+        }
+        return listaDocumente[index];
+    }
+};
+
+// Template Arhiva<T> cu shared_ptr pentru consistență
+template <typename T>
+class Arhiva {
+private:
+    vector<shared_ptr<T>> documente;
+public:
+    void adauga(shared_ptr<T> doc) {
+        documente.push_back(doc);
+    }
+
+    void afiseazaTot() const {
+        cout << "Continutul arhivei:\n";
+        for (const auto& doc : documente) {
+            doc->afiseaza();
+        }
+    }
+};
+
+// Funcția principală de test
+int main() {
+    Bibliotecar bibliotecar;
+
+    // Creăm documente fictive și le adăugăm
+    shared_ptr<Document> c1 = make_shared<Carte>("Umbra Codrilor", "Lavinia Petrescu", 412);
+    shared_ptr<Document> r1 = make_shared<Revista>("Stiinta Azi", 15);
+    shared_ptr<Document> z1 = make_shared<Ziar>("Gazeta Aurora", "29.06.2025");
+
+    bibliotecar.adaugaDocument(c1);
+    bibliotecar.adaugaDocument(r1);
+    bibliotecar.adaugaDocument(z1);
+
+    cout << "\nDocumente din biblioteca:\n";
+    bibliotecar.afiseazaDocumente();
+
+    // Testăm operatorul []
+    cout << "\nAccesam documentul cu index 1:\n";
+    bibliotecar[1]->afiseaza();
+
+    // Folosim template-ul Arhiva<T>
+    Arhiva<Carte> arhivaCarti;
+    shared_ptr<Carte> carte1 = make_shared<Carte>("Drumul Stelelor", "Victor Marin", 230);
+    arhivaCarti.adauga(carte1);
+
+    cout << "\nArhiva cu carti:\n";
+    arhivaCarti.afiseazaTot();
+
+    return 0;
+}
